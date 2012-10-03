f17322a6816a523cf5a1c494cf1e4f340eaa950e
Dennis Zierahn <dezi@dezis-macMini.local>
2012-10-03 17:19:19 +0200

    Check in.

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-console.log('pupso');
+console.log('readapp');
 
-var match = document.location.href.match(/(http|https):\/\/(.+)/);
+var match = document.location.href.match(/(http|https):\/\/([^\/?]+)/);
 
 console.log(match);
+
+var site = document.createElement('script');
+
+site.type = 'text/javascript'
+
+site.src  
+	= 'https://raw.github.com'
+	+ '/dezi/readapp/master'
+	+ '/sites/'
+	+ match[ 2 ]
+	+ '.js'
+	;
+
+document.body.appendChild(site);
 
